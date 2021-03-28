4325fd18d0655e0e8ebee5dc2bcf7849896f4658
teixeiramarcia <filipa.teixeira98@gmail.com>
2021-03-28 12:19:58 +0100

    Implement a decent html page

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -20,22 +20,21 @@ dest_file.write(" <head>\n")
 dest_file.write("     <meta charset='UTF-8'/>\n")
 dest_file.write("     <title>TP1</title>\n")
 dest_file.write(" </head>\n")
-dest_file.write("     <body>\n")
-dest_file.write("       <h1>Machine Learning: Datasets de treino</h1>")
+dest_file.write("     <body style='background-color: #e6e6ff;'>\n")
+dest_file.write("        <h2 style='color: #633b97'>Machine Learning: Datasets de treino</h2>\n")
 
 for linha in file:
     y = re.match(r'(B|I)-([a-zA-Z_]+)\t+([a-zA-Z0-9]+)|(O).+', linha)
     if y:
         if (y.group(1) == 'B'):
             if (in_category == 1):
-                dest_file.write("        " + "<p>" + category + sentence + "</p>\n")
-                dest_file.write("        <p>Elements: " + str(num_elems) + "</p>\n")
+                dest_file.write("        " + "<p>" + category + sentence + "  (elements: " + str(num_elems) + ")</p>")
                 num_elems = 0
                 category = ""
                 sentence = ""
             in_category = 1
             num_elems += 1
-            category = "<strong>" + y.group(2) + ": </strong>"
+            category = "<strong><a href='https://www.google.com/' style='color: #535353'>" + y.group(2) + "</a>" + ": </strong>"
             sentence = y.group(3) + ' '
             
         elif (y.group(1) == 'I'):
@@ -43,15 +42,13 @@ for linha in file:
             sentence += y.group(3) + ' '
         elif (y.group(4) == 'O'):
             if (in_category == 1):
-                dest_file.write("        " + "<p>" + category + sentence + "</p>\n")
-                dest_file.write("        <p>Elements: " + str(num_elems) + "</p>\n")
+                dest_file.write("        " + "<p>" + category + sentence + "  (elements: " + str(num_elems) + ")</p>")
                 num_elems = 0
                 category = ""
                 sentence = ""
                 in_category = 0
     else:
-        dest_file.write("        " + "<p>" + category + sentence + "</p>\n")
-        dest_file.write("        <p>Elements: " + str(num_elems) + "</p>\n")
+        dest_file.write("        " + "<p>" + category + sentence + "  (elements: " + str(num_elems) + ")</p>")
         num_elems = 0
         category = ""
         sentence = ""
