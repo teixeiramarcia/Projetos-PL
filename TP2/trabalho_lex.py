d50d493118a351918ac58a8bc72ae3acdd393744
PSilva3 <36642980+PSilva3@users.noreply.github.com>
2021-05-18 18:15:56 +0100

    First Version - Grammer, lex, yacc

---
+++ TP2/trabalho_lex.py
@@ -0,0 +1,43 @@
+import ply.lex as lex
+
+tokens = ['INT', 'ID', 'IF', 'ELSE', 'ELIF', 'REPEAT', 'READ', 'PRINT', 'NUM', 'RETURN'] 
+literals = ['(', ')',';','<','>','=','+','-','*','/','{','}','[',']','|']
+
+def t_IF(t):
+    r'if'
+
+def t_ELIF(t):
+    r'elif'
+
+def t_ELSE(t):
+    r'else'
+
+def t_REPEAT(t):
+    r'repeat'
+
+def t_RETURN(t):
+    r'return'
+
+def t_READ(t):
+    r'scan'
+
+def t_PRINT(t):
+    r'print'
+
+def t_INT(t):
+    r'int'
+
+def t_NUM(t):
+    r'\d+'
+
+def t_ID(t):
+    r'[a-z][a-zA-Z0-9]+'
+
+t_ignore = " \t\n"
+
+def t_error(t):
+    print('Caráter ilegal: ', t.value[0])
+    t.lexer.skip(1)
+
+#Build the Lexer
+lexer = lex.lex()
---
+++ TP2/trabalho_yacc.py
@@ -0,0 +1,146 @@
+import ply.yacc as yacc
+import sys
+
+#Get the token from 
+from trabalho_lex import tokens, literals
+
+#Producion rules
+def p_Code(p):
+    "CODE : DECLS INSIDE"
+    #p_DECLS(p[1])
+    #p_INSIDE(p[2])
+    pass
+
+def p_DECLS_DECL(p):
+    "DECLS : DECLS DECL ';'"
+    #p_DECLS(p[1])
+    #p_DECL(p[2])
+    pass
+
+def p_DECLS_DECLA(p):
+    "DECLS : DECLS DECLA ';'"
+    #p_DECLS(p[1])
+    #p_DECLA(p[2])
+    pass
+
+def p_DECLS_VAZIO(p):
+    "DECLS : Vazio"
+    pass
+
+def p_DECL_ID(p):
+    "DECL : INT ID"
+    if (p[2] in p.parser.variables.keys()):
+        print("Erro, na linha ", lines)
+        print("A variavel ", p[2], " já existe")
+    else:
+        p.parser.variables[p[2]] = offset_var
+        offset_var += 1
+        print("PUSHI 0")
+
+def p_DECL_VALOR(p):
+    "DECL : INT ID '=' VALOR"
+    if (p[2] in p.parser.variables.keys()):
+        print("Erro na linha ", lines)
+        print("A variavel ", p[2], " já existe")
+    else:
+        p.parser.variables[p[2]] = offset_var
+        offset_var += 1
+        print("PUSHI ", p[3])
+
+def p_DECLA_NUM(p):
+    "DECLA : INT ID '[' NUM ']'"
+    if (p[2] in p.parser.arrays.keys()):
+        print("Erro na linha ", lines)
+        print("A variavel ", p[2], " já existe")
+    else:
+        p.parser.arrays[p[2]] = (offset_var, offset_var+p[4])
+        offset_var += p[4]
+        print("PUSHN 0")
+
+def p_DECLA_ID(p):
+    "DECLA : INT ID '[' ID ']'"
+    if (p[2] in p.parser.arrays.keys()):
+        print("Erro na linha ", lines)
+        print("A variavel ", p[2], " já existe")
+    elif (p[4] not in p.parser.variables.keys()):
+        print("Erro na linha ", lines)
+        print("A variavel", p[4], " não existe")
+    elif (p.parser.variables.get(p[2]) < p.parser.arrays.get(p[4])[0] || p.parser.variables.get(p[2]) > p.parser.arrays.get(p[4])[1]):
+        print("Erro na linha ", lines)
+        print("O valor da variavel", p[4], " está fora do tamanho do array")
+    else:
+        p.parser.arrays[p[2]] = (offset_var, offset_var+p.registers.variables.get(p[4]))
+        offset_var += p[3]
+        print("PUSHN ", p[3])
+
+def p_DECLA_NUM(p):
+    "DECLA : INT ID '[' NUM ']'"
+    p.parser.arrays[p[2]] = (offset_var, offset_var+p[3])
+    offset_var += p[3]
+    print("PUSHN ", p[3])
+
+def p_Comando_dump(p):
+    "Comando : DUMP"
+    print(p.parser.registers) 
+
+def p_Exp_add(p):
+    "Exp : '(' ADD Exp Termo ')'"
+    p[0] = p[3] + p[4]
+
+def p_Exp_sub(p):
+    "Exp : '(' SUB Exp Termo ')'"
+    p[0] = p[3] - p[4]
+
+def p_Exp_termo(p):
+    "Exp : Termo"
+    p[0] = p[1]
+
+def p_Termo_mul(p):
+    "Termo : '(' MUL Termo Factor ')'"
+    p[0] = p[3] * p[4]
+
+def p_Termo_div(p):
+    "Termo : '(' DIV Termo Factor ')'"
+    if(p[4!=0]):
+        p[0] = p[3] / P[4]
+    else:
+        print("Erro: divisão por 0. A continuar com o dividendo: ", p[3])
+        p[0] = p[3]
+    
+def p_Termo_factor(p):
+    "Termo : Factor"
+    p[0] = p[1]
+
+def p_Factor_group(p):
+    "Factor : '(' Exp ')'"
+    p[0] = p[2]
+
+def p_Factor_num(p):
+    "Factor : NUM"
+    p[0] = int(p[1])
+
+def p_Factor_id(p):
+    "Factor : ID"
+    p[0] = p.parser.registers.get(p[1])
+
+def p_error(p):
+    print("Syntax error in input: ", p)
+
+#Build the parser
+parser = yacc.yacc()
+
+#My state
+parser.variables = {}
+parser.arrays = {}
+offset_var = 0
+offset_arr = 0
+lines = 1
+
+#Read line input and parse it
+import sys
+for linha in sys.stdin:
+    result = parser.parse(linha)
+    lines += 1
+    print(result)
+
+# TIRAR TUDO DE CAPSLOCK!!!!!!!!!!!!!!!!!!!!!!!!!
