fdc413dfc2ab178125bd6bd5413b9c56cd7ccd2f
PSilva3 <36642980+PSilva3@users.noreply.github.com>
2021-05-26 13:19:31 +0100

    Remove grammer circularity, add arrays

--- TP2/trabalho_lex.py
+++ TP2/trabalho_lex.py
@@ -1,23 +1,28 @@
 import ply.lex as lex
 
-tokens = ['INT', 'ID', 'IF', 'ELSE', 'ELIF', 'REPEAT', 'READ', 'PRINT', 'NUM', 'RETURN']
-literals = ['(', ')',';','<','>','=','+','-','*','/','{','}','[',']','|','&']
+tokens = ['INT', 'ID', 'IF', 'ELSE', 'ELIF', 'REPEAT', 'READ', 'PRINT', 'NUM', 'RETURN', 'COMENTARIO', 'NOT', 'TRUE', 'FALSE']
+literals = ['(', ')',';','<','>','=','+','-','*','/','{','}','[',']','|','&','!']
 
-#def t_IF(t):
-#    r'if'
-#
-#def t_ELIF(t):
-#    r'elif'
-#
-#def t_ELSE(t):
-#    r'else'
-#
-#def t_REPEAT(t):
-#    r'repeat'
-#
+def t_IF(t):
+    r'if'
+    return t
+
+def t_ELIF(t):
+    r'elif'
+    return t
+
+def t_ELSE(t):
+    r'else'
+    return t
+
+def t_REPEAT(t):
+    r'repeat'
+    return t
+
 #def t_RETURN(t):
 #    r'return'
 #
+
 def t_READ(t):
     r'read'
     return t
@@ -30,12 +35,28 @@ def t_INT(t):
     r'int'
     return t
 
+def t_NOT(t):
+    r'not'
+    return t
+
+def t_TRUE(t):
+    r'true'
+    return t
+
+def t_FALSE(t):
+    r'false'
+    return t
+
 def t_NUM(t):
     r'\d+'
     return t
 
 def t_ID(t):
     r'[a-z][a-zA-Z0-9]*'
+    return t
+
+def t_COMENTARIO(t):
+    r'(\/\/.+)|(/\*((?!\*/).|\n)*\*/)'
     return t
 
 t_ignore = " \t\n\r"
--- TP2/trabalho_yacc.py
+++ TP2/trabalho_yacc.py
@@ -1,4 +1,5 @@
 import ply.yacc as yacc
+import re
 import sys
 
 #Get the token from 
@@ -7,106 +8,135 @@ from trabalho_lex import tokens, literals, lexer
 #Producion rules
 def p_Code(p):
     "CODE : DECLS INSIDE"
-    print("START")
-    print(p[1], end='')
-    #print(p[2], end='')
-    print("STOP")
+    file = open("../vms-vf/vms/trial.vm", "w")
+    file.write("START\n")
+    file.write(p[1])
+    file.write(p[2])
+    file.write("STOP\n")
+    file.close()
 
-def p_DECLS_DECLS1(p):
+def p_DECLS_DECL(p):
     "DECLS : DECLS DECL ';'"
     p[0] = p[1] + p[2]
 
-def p_DECLS_DECL(p):
-    "DECLS : DECL ';'"
-    parser.lines += 1
+def p_DECLS_DECLA(p):
+    "DECLS : DECLS DECLA ';'"
+    p[0] = p[1] + p[2]
+
+def p_DECLS_COMENTARIO(p):
+    "DECLS : DECLS COMENTARIO"
     p[0] = p[1]
 
-#def p_DECLS_DECLS2(p):
-#    "DECLS : DECLS DECLA ';'"
-#    p[0] = p[1] + p[2]
-
-#def p_DECLS_DECLA(p):
-#    "DECLS : DECLA ';'"
-#    p[0] = p[1]
+def p_DECLS_VAZIO(p):
+    "DECLS : "
+    p[0] = ""
 
 def p_INSIDE_INSIDE(p):
     "INSIDE : INSIDE INFO"
     p[0] = p[1] + p[2]
+    #p[0] = ""
 
 def p_INSIDE_INFO(p):
-    "INSIDE : INFO"
+    "INSIDE : "
+    p[0] = ""
+
+def p_DECL_ID(p):
+    "DECL : INT ID"
+    if (p[2] in parser.variables.keys()):
+        print("Erro na linha ", parser.lines)
+        print("A variavel ", p[2], " já existe")
+    else:
+        parser.variables.update({p[2]: parser.offset_var})
+        parser.offset_var += 1
+        p[0] = "PUSHI 0\n"
+
+def p_DECL_VALOR(p):
+    "DECL : INT ID '=' MATH"
+    if (p[2] in parser.variables.keys()):
+        print("Erro na linha ", parser.lines)
+        print("A variavel ", p[2], " já existe")
+    else:
+        parser.variables.update({p[2]: parser.offset_var})
+        parser.offset_var += 1
+        if(re.match(r'PUSHI \d+\n',p[4])):
+            p[0] = p[4]
+        else:
+            p[0] = "PUSHI 0\n" + p[4] + "STOREG " + str(parser.offset_var-1) + "\n"
+
+def p_DECLA(p):
+    "DECLA : INT ID '[' NUM ']'"
+    if (p[2] in parser.arrays.keys()):
+        print("Erro na linha ", parser.lines)
+        print("A variavel ", p[2], " já existe")
+    else:
+        parser.arrays.update({p[2]: [parser.offset_var,parser.offset_var+int(p[4])-1]})
+        parser.offset_var += int(p[4])
+        p[0] = "PUSHN "+p[4]+"\n"
+
+def p_INFO_ATUL(p):
+    "INFO : ATUL ';'"
     p[0] = p[1]
 
-#def p_INFO_ATUL(p):
-#    "INFO : ATUL ';'"
-#    p[0] = p[1]
+def p_INFO_IFBLOCK(p):
+    "INFO : IFBLOCK"
+    p[0] = p[1]
 
-#def p_INFO_MATH(p):
-#    "INFO : MATH ';'"
-#    p[0] = p[1]
-
-#def p_INFO_IFBLOCK(p):
-#    "INFO : IFBLOCK"
-#    p[0] = p[1]
-
-#def p_INFO_REPEATBLOCK(p):
-#    "INFO : REPEATBLOCK"
-#    p[0] = p[1]
+def p_INFO_REPEATBLOCK(p):
+    "INFO : REPEATBLOCK"
+    p[0] = p[1]
 
 def p_INFO_PRINT(p):
-    "INFO : PRINT '(' VALOR ')' ';'"
-    parser.lines += 1
-    p[0] = p[1] + "WRITEI\n"
+    "INFO : PRINT '(' MATH ')' ';'"
+    p[0] = p[3] + "WRITEI\n"
 
 #def p_INFO_RETURN(p):
 #    "INFO : RETURN VALOR ';'"
 #    p[0] = p[1]
 
+def p_INFO_COMENTARIO(p):
+    "INFO : COMENTARIO"
+    p[0] = ""
 
-def p_DECL_ID(p):
-    "DECL : INT ID"
-    if (p[2] in p.parser.variables.keys()):
-        print("Erro na linha ", parser.lines)
-        print("A variavel ", p[2], " já existe")
-    else:
-        p.parser.variables.update({p[2]: parser.offset_var})
-        parser.offset_var += 1
-        p[0] = "PUSHI 0\n"
-
-def p_DECL_VALOR(p):
-    "DECL : INT ID '=' VALOR"
-    if (p[2] in p.parser.variables.keys()):
-        print("Erro na linha ", parser.lines)
-        print("A variavel ", p[2], " já existe")
-    else:
-        p.parser.variables.update({p[2]: parser.offset_var})
-        parser.offset_var += 1
-        p[0] = "PUSHI 0\n" + p[4] + "STOREG " + str(parser.offset_var-1) + "\n"
-
-def p_VALOR_VALOR1(p):
-    "VALOR : VALOR1"
-    p[0] = p[1]
-
-def p_FACTOR_READ(p):
-    "VALOR : READ '(' ')'"
-    p[0] = "READ\nATOI\n"
-
-def p_VALOR1_NUM(p):
-    "VALOR1 : NUM"
-    p[0] = "PUSHI " + p[1] + "\n"
-
-def p_VALOR1_ID(p):
-    "VALOR1 : ID"
-    if(p[1] not in p.parser.variables.keys()):
+def p_ATUL_VAR(p):
+    "ATUL : ID '=' MATH"
+    if (p[1] not in parser.variables.keys()):
         print("Erro na linha ", parser.lines)
         print("A variavel ", p[1], " não existe")
     else:
-        res = p.parser.variables.get(p[1])
-        p[0] = "PUSHG " + str(res) + "\n"
+        p[0] = p[3] + "STOREG " + str(parser.variables.get(p[1])) + "\n"
 
-def p_VALOR1_MATH(p):
-    "VALOR1 : MATH"
-    p[0] = p[1]
+def p_ATUL_ARRAY(p):
+    "ATUL : ID '[' MATH ']' '=' MATH"
+    if (p[1] not in parser.arrays.keys()):
+        print("Erro na linha ", parser.lines)
+        print("A variavel ", p[1], " não existe")
+    else:
+        p[0] = "PUSHGP\n" + "PUSHI " + str(parser.arrays.get(p[1])[0]) + "\nPADD\n" + p[3] + p[6] + "STOREN\n"
+
+#def p_VALOR_VALOR1(p):
+#    "VALOR : VALOR1"
+#    p[0] = p[1]
+#
+#def p_VALOR_READ(p):
+#    "VALOR : READ '(' ')'"
+#    p[0] = "READ\nATOI\n"
+#
+#def p_VALOR1_NUM(p):
+#    "VALOR1 : NUM"
+#    p[0] = "PUSHI " + p[1] + "\n"
+#
+#def p_VALOR1_ID(p):
+#    "VALOR1 : ID"
+#    if(p[1] not in parser.variables.keys()):
+#        print("Erro na linha ", parser.lines)
+#        print("A variavel ", p[1], " não existe")
+#    else:
+#        res = parser.variables.get(p[1])
+#        p[0] = "PUSHG " + str(res) + "\n"
+#
+#def p_VALOR1_MATH(p):
+#    "VALOR1 : MATH"
+#    p[0] = p[1]
 
 def p_MATH_ADD(p):
     "MATH : MATH '+' TERMO"
@@ -132,20 +162,142 @@ def p_TERMO_FACTOR(p):
     "TERMO : FACTOR"
     p[0] = p[1]
 
-def p_FACTOR_VALOR(p):
-    "FACTOR : VALOR"
+def p_FACTOR_MATH(p):
+    "FACTOR : '(' MATH ')'"
+    p[0] = p[2]
+
+def p_FACTOR_NUM(p):
+    "FACTOR : NUM"
+    p[0] = "PUSHI " + p[1] + "\n"
+
+def p_FACTOR_ID(p):
+    "FACTOR : ID"
+    if(p[1] not in parser.variables.keys()):
+        print("Erro na linha ", parser.lines)
+        print("A variavel ", p[1], " não existe")
+    else:
+        res = parser.variables.get(p[1])
+        p[0] = "PUSHG " + str(res) + "\n"
+
+def p_FACTOR_ARRAY(p):
+    "FACTOR : ID '[' MATH ']'"
+    if (p[1] not in parser.arrays.keys()):
+        print("Erro na linha ", parser.lines)
+        print("A variavel ", p[1], " não existe")
+    else:
+        p[0] = "PUSHGP\n" + "PUSHI " + str(parser.arrays.get(p[1])[0]) + "\nPADD\n"
+
+def p_FACTOR_READ(p):
+    "FACTOR : READ '(' ')'"
+    p[0] = "READ\nATOI\n"
+
+def p_IFBLOCK(p):
+    "IFBLOCK : IF '(' COND ')' '{' INSIDE '}' IFBLOCK2"
+    if(p[8] == r"ELIF\d:(.|\n)+"):
+        p[0] = p[3] + "JZ ELIF" + str(parser.var_elif) + "\n" + p[6] + "JUMP FIMIF" + str(parser.var_fimif) + "\n" + p[8] + "FIMIF" + str(parser.var_fimif) + ":\n"
+    elif(p[8] == r"ELSE\d:(.|\n)+"):
+        p[0] = p[3] + "JZ ELSE" + str(parser.var_else) + "\n" + p[6] + "JUMP FIMIF" + str(parser.var_fimif) + "\n" + p[8] + "FIMIF" + str(parser.var_fimif) + ":\n"
+    else:
+        p[0] = p[3] + "JZ FIMIF" + str(parser.var_fimif) + "\n" + p[6] + "FIMIF" + str(parser.var_fimif) + ":\n"
+
+def p_IFBLOCK2_ELIF(p):
+    "IFBLOCK2 : ELIF '(' COND ')' '{' INSIDE '}' IFBLOCK2"
+    parser.var_elif+=1
+    if(p[8] == r"ELIF\d:(.|\n)+"):
+        p[0] = "ELIF" + str(parser.var_elif-1) +":\n" + p[3] + "JZ ELIF" + str(parser.var_elif) + "\n" + p[6] + "JUMP FIMIF" + str(parser.var_fimif) + "\n" + p[8]
+    elif(p[8] == r"ELSE\d:(.|\n)+"):
+        p[0] = "ELIF" + str(parser.var_elif-1) +":\n" + p[3] + "JZ ELSE" + str(parser.var_else) + "\n" + p[6] + "JUMP FIMIF" + str(parser.var_fimif) + "\n" + p[8]
+    else:
+        p[0] = "ELIF" + str(parser.var_elif-1) +":\n" + p[3] + "JZ FIMIF" + str(parser.var_fimif) + "\n" + p[6]
+
+def p_IFBLOCK2_ELSE(p):
+    "IFBLOCK2 : ELSE '{' INSIDE '}'"
+    parser.var_else += 1
+    p[0] = "ELSE" + str(parser.var_else-1) +":\n" + p[2]
+
+def p_IFBLOCK2_VAZIO(p):
+    "IFBLOCK2 : "
+    p[0] = ""
+
+def p_REPEATBLOCK(p):
+    "REPEATBLOCK : REPEAT '(' COND ')' '{' INSIDE '}'"
+    p[0] = ""
+
+def p_COND_AND(p):
+    "COND : COND '&' '&' COND1"
+    p[0] = p[1] + p[4] + "ADD\n"
+
+def p_COND_COND1(p):
+    "COND : COND1"
     p[0] = p[1]
 
-#def p_FACTOR_MATH(p):
-#    "FACTOR : MATH"
-#    p[0] = p[1]
+def p_COND1_OR(p):
+    "COND1 : COND1 '|' '|' COND2"
+    p[0] = p[1] + p[4] + "MUL\n"
 
-#def p_FACTOR_MATH1(p):
-#    "FACTOR : '(' MATH ')'"
-#    p[0] = p[1]
+def p_COND1_COND2(p):
+    "COND1 : COND2"
+    p[0] = p[1]
+
+def p_COND2_NOT(p):
+    "COND2 : NOT COND"
+    p[0] = p[1] + "NOT\n"
+
+def p_COND2_TRUE(p):
+    "COND2 : TRUE"
+    p[0] = 1
+
+def p_COND2_FALSE(p):
+    "COND2 : FALSE"
+    p[0] = 0
+
+def p_COND2_COND(p):
+    "COND2 : '(' COND ')'"
+    p[0] = p[2]
+
+def p_COND2_COND3(p):
+    "COND2 : COND3"
+    p[0] = p[1]
+
+def p_COND3_MENOR(p):
+    "COND3 : MATH '<' MATH"
+    p[0] = p[1] + p[3] + "INF\n"
+
+def p_COND3_MAIOR(p):
+    "COND3 : MATH '>' MATH"
+    p[0] = p[1] + p[3] + "SUP\n"
+    
+def p_COND3_IGUAL(p):
+    "COND3 : MATH '=' '=' MATH"
+    p[0] = p[1] + p[4] + "EQUALS\nNOT\n"
+    
+def p_COND3_DIF(p):
+    "COND3 : MATH '!' '=' MATH"
+    p[0] = p[1] + p[4] + "EQUALS\n"
+    
+def p_COND3_MAIORI(p):
+    "COND3 : MATH '<' '=' MATH"
+    p[0] = p[1] + p[4] + "SUPEQ\n"
+    
+def p_COND3_MENORI(p):
+    "COND3 : MATH '>' '=' MATH"
+    p[0] = p[1] + p[4] + "INFEQ\n"
+    
+#def p_COND1_OR(p):
+#    "COND1 : '|' '|' COND"
+#    p[0] = p[3] + "ADD\n"
+#
+#def p_COND1_AND(p):
+#    "COND1 : '&' '&' COND"
+#    p[0] = p[3] + "MUL\n"
+#
+#def p_COND1_VAZIO(p):
+#    "COND1 : "
+#    p[0] = ""
+
 
 def p_error(p):
-    print(parser.lines)
+    print(p)
     print("Syntax error in input!")
 
 #Build the parser
@@ -156,10 +308,17 @@ parser.variables = {}
 parser.arrays = {}
 parser.offset_var = 0
 parser.offset_arr = 0
+parser.var_elif = 0
+parser.var_fimif = 0
 parser.lines = 1
 
 #Read line input and parse it
 
 f = open("trial.c", "r").read()
 
+#txt = f.split("\n")
+#print(txt)
+#for linha in txt:
+#    result = lexer.token()
+#    print(result)
 parser.parse(f)
